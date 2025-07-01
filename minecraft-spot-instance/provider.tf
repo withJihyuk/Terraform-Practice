7afd9509035bcab39bc9e2b8e0d9b0b1606e1b6c
Jihyuk Lee <lead@sver.dev>
2025-07-01 22:57:58 +0900

    feat :: aws provider 최신 버전으로 변경

--- minecraft-spot-instance/provider.tf
+++ minecraft-spot-instance/provider.tf
@@ -1,11 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+      source = "hashicorp/aws"
+      version = "6.0.0"
     }
   }
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
